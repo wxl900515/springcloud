77999ef74ab804b55c405cf22353529d9de9a198
wxl900515 <164603821@qq.com>
2019-11-21 21:26:27 +0800

    1

--- springcloud_service/src/main/java/com/wxl/app/controller/UserController.java
+++ springcloud_service/src/main/java/com/wxl/app/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.wxl.app.controller;
 
-public class UserController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wxl.app.entity.User;
+
+@RestController
+public class UserController {
+	
+	List<User>  list =  new  ArrayList<>();
+	
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Integer id) {
+		User us = new User();
+		us.setId(1);
+		us.setName("张三");
+		us.setAge(18);
+		us.setSex(0);
+		return us;
+	}
 }
