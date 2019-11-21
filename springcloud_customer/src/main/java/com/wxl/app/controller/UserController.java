b4f860eeb06e0a4022819f95054944cefc751964
wxl900515 <164603821@qq.com>
2019-11-21 21:26:15 +0800

    1

---
+++ springcloud_customer/src/main/java/com/wxl/app/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.wxl.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.wxl.app.entity.User;
+
+@RestController
+public class UserController {
+
+	
+	@Autowired
+	private RestTemplate  restTemplate;
+	
+	@Autowired
+	private LoadBalancerClient   loadBalancerClient;
+	
+	@GetMapping("/user/{id}")
+	public User findById(@PathVariable Integer id) {
+		
+		return this.restTemplate.getForObject("http://springcloud_service/"+id, User.class);
+		
+	}
+	
+	
+	@GetMapping("log-user-instance")
+	public void logUserInstance() {
+		
+		ServiceInstance  ss = this.loadBalancerClient.choose("springcloud_service");
+		System.out.println(ss.getServiceId());
+		System.out.println(ss.getHost());
+		System.out.println(ss.getPort());
+		
+	}
+	
+	
+}
--- springcloud_customer/src/main/java/com/wxl/app/entity/User.java
+++ springcloud_customer/src/main/java/com/wxl/app/entity/User.java
@@ -1,5 +1,54 @@
 package com.wxl.app.entity;
 
 public class User {
+	private  Integer id;
+	
+	private  String  name;
+	
+	private  Integer  age;
+	
+	private  Integer  sex;
+	
+	private  String  tel;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
 
 }
