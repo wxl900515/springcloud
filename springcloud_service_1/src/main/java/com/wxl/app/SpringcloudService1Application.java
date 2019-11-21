8a6cfb1c0edae25f0abbbd54844559a2de547810
wxl900515 <164603821@qq.com>
2019-11-21 22:39:48 +0800

    1

---
+++ springcloud_service_1/src/main/java/com/wxl/app/SpringcloudService1Application.java
@@ -0,0 +1,13 @@
+package com.wxl.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudService1Application.class, args);
+	}
+
+}
---
+++ springcloud_service_1/src/test/java/com/wxl/app/SpringcloudService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxl.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
