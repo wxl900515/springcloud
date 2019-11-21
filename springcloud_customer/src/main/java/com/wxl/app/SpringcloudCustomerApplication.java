9bd6b20f15c7433bafc9471bf12b134348f4c4fa
wxl900515 <164603821@qq.com>
2019-11-21 21:46:30 +0800

    1

--- springcloud_customer/src/main/java/com/wxl/app/SpringcloudCustomerApplication.java
+++ springcloud_customer/src/main/java/com/wxl/app/SpringcloudCustomerApplication.java
@@ -2,9 +2,19 @@ package com.wxl.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringcloudCustomerApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return  new RestTemplate();
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudCustomerApplication.class, args);
